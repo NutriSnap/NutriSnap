3919320a18a59e60b3c8a64d414af5b449255978
Michael Rogers <mlr2010@hawaii.edu>
2023-09-25 17:28:56 -1000

    Created basic starter template to build from

--- lib/main.dart
+++ lib/main.dart
@@ -1,121 +1,223 @@
 import 'package:flutter/material.dart';
-import 'package:nutrisnap/routing/route_generator.dart';
-import 'shared/themes.dart';
-import 'core/constants/colors.dart';
 
-void main() => runApp(const NutriSnapApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class NutriSnapApp extends StatelessWidget {
-  const NutriSnapApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'NutriSnap',
-      theme: AppTheme.lightTheme(),
-      darkTheme: AppTheme.darkTheme(),
-      initialRoute: '/',
-      onGenerateRoute: RouteGenerator.generateRoute,
+      theme: ThemeData(
+        colorScheme:
+            ColorScheme.fromSeed(seedColor: Colors.lightGreen.shade900),
+        useMaterial3: true,
+      ),
+      // Update to login page
+      home: const MySnapsPage(title: 'NutriSnap'),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
 
-// class NutriSnapApp extends StatefulWidget {
-//   const NutriSnapApp({super.key});
+class NavigationDrawer extends StatefulWidget {
+  const NavigationDrawer({super.key});
 
-//   @override
-//   State<NutriSnapApp> createState() => _NutriSnapAppState();
-// }
+  @override
+  State<NavigationDrawer> createState() => _NavigationDrawerState();
+}
 
-// class _NutriSnapAppState extends State<NutriSnapApp> {
-//   bool useMaterial3 = true;
-//   ThemeMode themeMode = ThemeMode.system;
-//   ColorSeed colorSelected = ColorSeed.baseColor;
-//   ColorImageProvider imageSelected = ColorImageProvider.leaves;
-//   ColorScheme? imageColorScheme = const ColorScheme.light();
-//   ColorSelectionMethod colorSelectionMethod = ColorSelectionMethod.colorSeed;
+class _NavigationDrawerState extends State<NavigationDrawer> {
+  int currentPageIndex = 0;
 
-//   bool get useLightMode {
-//     switch (themeMode) {
-//       case ThemeMode.system:
-//         return View.of(context).platformDispatcher.platformBrightness ==
-//             Brightness.light;
-//       case ThemeMode.light:
-//         return true;
-//       case ThemeMode.dark:
-//         return false;
-//     }
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: NavigationBar(
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPageIndex = index;
+          });
+        },
+        indicatorColor: Colors.amber[800],
+        selectedIndex: currentPageIndex,
+        destinations: const <Widget>[
+          NavigationDestination(
+            selectedIcon: Icon(Icons.dashboard),
+            icon: Icon(Icons.dashboard_outlined),
+            label: 'Dashboard',
+          ),
+          NavigationDestination(
+            icon: Icon(Icons.description_outlined),
+            label: 'Journal',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.insert_chart),
+            icon: Icon(Icons.insert_chart_outlined),
+            label: 'Trends',
+          ),
+          NavigationDestination(
+            selectedIcon: Icon(Icons.emoji_events),
+            icon: Icon(Icons.emoji_events_outlined),
+            label: 'Challenges',
+          ),
+        ],
+      ),
+      body: <Widget>[
+        Container(
+          color: Colors.red,
+          alignment: Alignment.center,
+          child: const Text('Page 1'),
+        ),
+        Container(
+          color: Colors.green,
+          alignment: Alignment.center,
+          child: const Text('Page 2'),
+        ),
+        Container(
+          color: Colors.blue,
+          alignment: Alignment.center,
+          child: const Text('Page 3'),
+        ),
+        Container(
+          color: Colors.yellow,
+          alignment: Alignment.center,
+          child: const Text('Page 4'),
+        ),
+      ][currentPageIndex],
+    );
+  }
+}
 
-//   void handleBrightnessChange(bool useLightMode) {
-//     setState(() {
-//       themeMode = useLightMode ? ThemeMode.light : ThemeMode.dark;
-//     });
-//   }
+// Login Page
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
 
-//   // void handleMaterialVersionChange() {
-//   //   setState(() {
-//   //     useMaterial3 = !useMaterial3;
-//   //   });
-//   // }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NutriSnap'),
+      ),
+      body: const Center(
+        child: Text('Login Page'),
+      ),
+    );
+  }
+}
 
-//   void handleColorSelect(int value) {
-//     setState(() {
-//       colorSelectionMethod = ColorSelectionMethod.colorSeed;
-//       colorSelected = ColorSeed.values[value];
-//     });
-//   }
+// Sign Up Page
+class SignUpPage extends StatelessWidget {
+  const SignUpPage({super.key});
 
-//   void handleImageSelect(int value) {
-//     final String url = ColorImageProvider.values[value].url;
-//     ColorScheme.fromImageProvider(provider: NetworkImage(url))
-//         .then((newScheme) {
-//       setState(() {
-//         colorSelectionMethod = ColorSelectionMethod.image;
-//         imageSelected = ColorImageProvider.values[value];
-//         imageColorScheme = newScheme;
-//       });
-//     });
-//   }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NutriSnap'),
+      ),
+      body: const Center(
+        child: Text('Sign Up Page'),
+      ),
+    );
+  }
+}
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       title: 'Material 3',
-//       themeMode: themeMode,
-//       theme: ThemeData(
-//         colorSchemeSeed: colorSelectionMethod == ColorSelectionMethod.colorSeed
-//             ? colorSelected.color
-//             : null,
-//         colorScheme: colorSelectionMethod == ColorSelectionMethod.image
-//             ? imageColorScheme
-//             : null,
-//         useMaterial3: useMaterial3,
-//         brightness: Brightness.light,
-//       ),
-//       darkTheme: ThemeData(
-//         colorSchemeSeed: colorSelectionMethod == ColorSelectionMethod.colorSeed
-//             ? colorSelected.color
-//             : imageColorScheme!.primary,
-//         useMaterial3: true,
-//         brightness: Brightness.dark,
-//       ),
-//       initialRoute: '/',
-//       onGenerateRoute: RouteGenerator.generateRoute,
+// Welcome Page (after login)
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
 
-//       // home: Home(
-//       //   useLightMode: useLightMode,
-//       //   useMaterial3: useMaterial3,
-//       //   colorSelected: colorSelected,
-//       //   imageSelected: imageSelected,
-//       //   handleBrightnessChange: handleBrightnessChange,
-//       //   handleMaterialVersionChange: handleMaterialVersionChange,
-//       //   handleColorSelect: handleColorSelect,
-//       //   handleImageSelect: handleImageSelect,
-//       //   colorSelectionMethod: colorSelectionMethod,
-//       // ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('NutriSnap'),
+      ),
+      body: const Center(
+        child: Text('Welcome Page'),
+      ),
+    );
+  }
+}
+
+class MySnapsPage extends StatefulWidget {
+  const MySnapsPage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MySnapsPage> createState() => _MySnapsPageState();
+}
+
+class _MySnapsPageState extends State<MySnapsPage> {
+  // image
+  static const String _image = 'assets/images/layout/foodplaceholder.png';
+
+  void _snapPicture() {
+    setState(() {
+      // save the image and display it
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        // gradiant background
+        backgroundColor: Colors.transparent,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: <Color>[
+                Color(0xFF13E3D2),
+                Color(0xFF5D74E2),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+        ),
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            // Show the image from the assets folder
+            Image.asset(_image),
+            const Text(
+              'Label your Snap',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const Text(
+              'Take a picture of your food to get nutritional information!',
+            ),
+            const Text(
+              'Enter your food here:',
+            ),
+            const TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Enter a food item',
+              ),
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _snapPicture,
+        tooltip: 'Increment',
+        child: const Icon(Icons.camera_alt_rounded,
+            color: Colors.black87, size: 40),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: const NavigationDrawer(),
+    );
+  }
+}
