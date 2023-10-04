2e6e069defe15956398c217d8c0e2f7f78e4d5b7
Lydia Sollis <lydia.j.sollis@gmail.com>
2023-10-04 12:30:25 -1000

    challenges card list

--- lib/views/challenges/challenges_screen.dart
+++ lib/views/challenges/challenges_screen.dart
@@ -1,4 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:nutrisnap/views/challenges/widgets/challenges_card.dart';
+
+List<ChallengesCard> _buildGridCards(int count) {
+  List<ChallengesCard> cards = List.generate(
+    count,
+        (int index) {
+      return const ChallengesCard();
+    },
+  );
+  return cards;
+}
 
 class ChallengesScreen extends StatelessWidget {
   const ChallengesScreen({Key? key}) : super(key: key);
@@ -7,12 +18,12 @@ class ChallengesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Challenges Screen',
-          style: TextStyle(fontSize: 24),
-        ),
+    return Scaffold(
+      body: GridView.count(
+        crossAxisCount: 1,
+        padding: const EdgeInsets.all(16.0),
+        childAspectRatio: 16.0 / 9.0,
+        children: _buildGridCards(4),
       ),
     );
   }
