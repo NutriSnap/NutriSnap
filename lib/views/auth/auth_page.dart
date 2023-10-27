003ee7918a11395fd9affb7e35aa8942e8afc66e
Michael Rogers <mlr2010@hawaii.edu>
2023-10-26 23:53:17 -1000

    might remove - revisit on firebase setup

--- lib/views/auth/auth_page.dart
+++ lib/views/auth/auth_page.dart
@@ -1,25 +1,25 @@
-// import 'package:flutter/material.dart';
-// import 'package:nutrisnap/views/auth/toggle_signon_page.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:nutrisnap/views/dashboard/dashboard_page.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:nutrisnap/views/auth/signup_page.dart';
+import '../../common/main_scaffold.dart';
 
-// class AuthPage extends StatelessWidget {
-//   const AuthPage({Key? key}) : super(key: key);
+class AuthPage extends StatelessWidget {
+  const AuthPage({Key? key}) : super(key: key);
 
-//   static const String routeName = '/authenticate';
+  static const String routeName = '/authenticate';
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: StreamBuilder<User?>(
-//           stream: FirebaseAuth.instance.authStateChanges(),
-//           builder: (context, snapshot) {
-//             if (snapshot.hasData) {
-//               return const DashboardPage();
-//             } else {
-//               return const LoginOrRegisterPage();
-//             }
-//           }),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: StreamBuilder<User?>(
+          stream: FirebaseAuth.instance.authStateChanges(),
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              return MainScaffold();
+            } else {
+              return const SignupPage();
+            }
+          }),
+    );
+  }
+}
