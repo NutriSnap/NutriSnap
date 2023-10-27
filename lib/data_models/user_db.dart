07c0a5f89f591ee8d92e0e255045d12ac460ffbf
Michael Rogers <mlr2010@hawaii.edu>
2023-10-26 23:55:24 -1000

    convert userDb to riverpod provider

--- lib/data_models/user_db.dart
+++ lib/data_models/user_db.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nutrisnap/data_models/user_challenge.dart';
 import 'package:nutrisnap/data_models/user_badge.dart';
 import 'meal.dart';
@@ -9,17 +10,55 @@ class UserData {
     required this.username,
     required this.email,
     this.imagePath,
+    this.phone,
     required this.initials,
   });
   String id;
   String name;
   String username;
   String email;
-  String? imagePath;
   String initials;
+  String? imagePath;
+  String? phone;
+  // String? location = '';
+  // String? role = '';
+  // String bio = '';
+  // bool isVerified = false;
+  // List<String>? followers = [];
+  // List<String>? following = [];
+  // List<String>? savedMeals = [];
+  // List<String>? savedSnaps = [];
+  // List<String>? savedChallenges = [];
+  // List<String>? savedBadges = [];
+
+  UserData copyWith({
+    String? id,
+    String? name,
+    String? username,
+    String? email,
+    String? imagePath,
+    String? phone,
+    String? initials,
+    // ... any other optional fields you want to include ...
+  }) {
+    return UserData(
+      id: id ?? this.id,
+      name: name ?? this.name,
+      username: username ?? this.username,
+      email: email ?? this.email,
+      imagePath: imagePath ?? this.imagePath,
+      phone: phone ?? this.phone,
+      initials: initials ?? this.initials,
+      // ... other fields ...
+    );
+  }
 }
 
 class UserDB {
+  UserDB(this.ref);
+
+  final ProviderRef<UserDB> ref;
+
   final List<UserData> _users = [
     UserData(
       id: 'user-001',
@@ -27,6 +66,7 @@ class UserDB {
       username: '@fluke',
       email: 'jennacorindeane@gmail.com',
       imagePath: 'assets/images/user-001.jpg',
+      phone: '808-123-4567',
       initials: 'JD',
     ),
     UserData(
@@ -34,6 +74,7 @@ class UserDB {
       name: 'Jenna Smith',
       username: '@fluke',
       email: 'j_smith@hawaii.edu',
+      phone: '',
       imagePath: 'assets/images/user-002.jpg',
       initials: 'JS',
     ),
@@ -43,16 +84,29 @@ class UserDB {
       username: '@katie',
       email: 'katie.y@gmail.com',
       imagePath: 'assets/images/user-003.jpg',
+      phone: '808-123-4567',
       initials: 'KY',
     )
   ];
 
-  UserData getUser(String userId) {
-    return _users.firstWhere((user) => user.id == userId);
+  bool doesUserExist(String id) {
+    return _users.any((u) => u.id == id);
+  }
+
+  UserData? getUser(String id) {
+    return _users.firstWhere((u) => u.id == id);
   }
 
   List<String> getUserIDs() {
     return _users.map((user) => user.id).toList();
+  }
+
+  List<UserData> getUsers() {
+    return _users.toList();
+  }
+
+  String getUserByUsername(String username) {
+    return _users.firstWhere((user) => user.username == username).id;
   }
 
   List<String> getAssociatedMealIds(String userId) {
@@ -66,10 +120,37 @@ class UserDB {
   List<String> getAssociatedUserBadgeIds(String userId) {
     return userBadgeDB.getUserBadgeIdsByUser(userId);
   }
+
+  void addUser(UserData user) {
+    _users.add(user);
+  }
+
+  void updateUser(UserData user) {
+    if (!doesUserExist(user.id)) {
+      throw Exception('User with id ${user.id} not found.');
+    }
+
+    UserData oldUser = getUser(user.id)!;
+    UserData updatedUser = oldUser.copyWith(
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      imagePath: user.imagePath,
+      phone: user.phone,
+      initials: user.initials,
+      // ... other fields ...
+    );
+
+    int index = _users.indexWhere((u) => u.id == user.id);
+    _users[index] =
+        updatedUser; // Replace the old user with the updated user in the list
+  }
 }
 
-/// The singleton instance providing access to all user data for clients.
-UserDB userDB = UserDB();
+final userDBProvider = Provider<UserDB>((ref) {
+  return UserDB(ref);
+});
 
-/// The currently logged in user.
-String currentUserId = 'user-003';
+final currentUserIDProvider = StateProvider<String>((ref) {
+  return 'user-003';
+});
